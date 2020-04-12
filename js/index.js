18bb2b6475e107d2bea8787cc7d12ef77884d175
bishopwm <bishopwm@g.cofc.edu>
2020-04-12 13:31:04 -0400

    clean up code indentations and notes, add country flags, add covid virus with motion, add side panel with current position

--- js/index.js
+++ js/index.js
@@ -1,122 +1,144 @@
 window.onload = () => {
     document.getElementById('start-button').onclick = () => {
-      document.getElementById('canvas').style = "visibility: visible;"
+      document.getElementById('canvas').style = "visibility: visible;";
+      document.getElementById('sidebar').style = "visibility: visible;"
       startGame();
       animate();
     };
   
-      // Grab canvas element in HTML and set dimensions:
-      const canvas = document.getElementById('canvas');
-      canvas.width = 1200;
-      canvas.height = 800;
+    // GRAB CANVAS ELEMENT IN HTML/SET DIMENSIONS
+    const canvas = document.getElementById('canvas');
+    canvas.width = 1100;
+    canvas.height = 800;
 
-      // Canvas boilerplate code:
-      const ctx = canvas.getContext('2d');
-      
-      // Use as cancel button: 
-      let animateId = null;
+    // CANVAS BOILER PLATE CODE
+    const ctx = canvas.getContext('2d');
+    
+    let animateId = null;
 
-      // declare images and drawings:
-    //   let avatarimage = new Image()
-    //   avatarimage.src = 'images/explorer.png'
+    // CREATE NEW IMAGES
+    let avatarimage = new Image()
+    avatarimage.src = 'images/avatar.png'
 
-        let avatarimage = new Image()
-        avatarimage.src = 'images/avatar.png'
+    let covidimage = new Image()
+    covidimage.src = 'images/covid.png'
 
-        let countryUSAimage = new Image()
-        countryUSAimage.src = 'images/countryUSA.png'
+    let countryUSAimage = new Image()
+    countryUSAimage.src = 'images/american-flag.png'
 
-        let countryUKimage = new Image()
-        countryUKimage.src = 'images/countryUK.jpg'
+    let countryUKimage = new Image()
+    countryUKimage.src = 'images/countryUK.jpg'
 
-        let countryInimage = new Image()
-        countryInimage.src = 'images/countryIn.png'
-      
-    //   function drawSquare(){
-    //       ctx.fillStyle="green";
-    //       ctx.fillRect(15, 15, 15, 15); 
-    //   }
+    let countryIndiaimage = new Image()
+    countryIndiaimage.src = 'images/countryIn.png'
 
-      function drawAvatar(){
-          ctx.drawImage(avatar.image, avatar.x, avatar.y, 20, 20)
-      }
-      function drawCountryUSA(){
-          ctx.drawImage(countryUSA.image, countryUSA.x, countryUSA.y, 60, 60)
-      }
+    let countryAUSimage = new Image()
+    countryAUSimage.src = 'images/countryAUS.png'
+    
 
-      function drawCountryUK(){
-        ctx.drawImage(countryUK.image, countryUK.x, countryUK.y, 35, 25)
+    function recordPosition(){
+        document.getElementById('current-position').innerHTML = "Current Position:  " + avatar.x +",  "+ avatar.y
     }
-  
-      function drawCountryIn(){
-        ctx.drawImage(countryIn.image, countryIn.x, countryIn.y, 35, 25)  
-      }
 
+    function drawCovids(){   
+    covids.forEach((covid) => { 
+        ctx.drawImage(covid.image, covid.x--, covid.y--, 50, 40)
+        })
+    }
 
-      //declare avatar object
-      let avatar = {
-          x: 10,
-          y: 10,
-          image: avatarimage
-      }
+    // DRAW COVID ICONS
+    let covids = []       
+    setInterval(function(){
+    let covid = {
+        x: 1000,
+        y: 790,
+        image: covidimage
+    }
+    if(covids.length <= 25){
+        covids.push(covid);
+    }
+    }, 1000)
 
+    // DRAW AVATAR
+    function drawAvatar(){
+        ctx.drawImage(avatar.image, avatar.x, avatar.y, 20, 20)
+    }
 
+    // DRAW INDIVIDUAL COUNTRIES
+    function drawCountryUSA(){
+        ctx.drawImage(countryUSA.image, countryUSA.x, countryUSA.y, 50, 30)
+    }
+    function drawCountryUK(){
+        ctx.drawImage(countryUK.image, countryUK.x, countryUK.y, 50, 30)
+    }
+    function drawCountryIndia(){
+        ctx.drawImage(countryIndia.image, countryIndia.x, countryIndia.y, 50, 30)  
+    }
+    function drawCountryAUS(){
+        ctx.drawImage(countryAUS.image, countryAUS.x, countryAUS.y, 50, 30)  
+    }
 
-      //declare country USA object
-      let countryUSA = {
-          x: 200,
-          y: 220,
-          image: countryUSAimage
-      }
-
-      //declare country UK object
-      let countryUK = {
-          x: 650,
-          y: 135,
-          image: countryUKimage
-      }
-
-      let countryIn = {
-          x: 1050,
-          y: 300,
-          image: countryInimage
-      }
-
-      document.body.onkeypress = function(e){
-        if(e.key === 'w'){ //Move up
+    // DECLARE OBJECTS - AVATAR
+    let avatar = {
+        x: 10,
+        y: 10,
+        image: avatarimage
+    }
+    // DECLARE OBJECTS - COUNTRIES
+    let countryUSA = {
+        x: 170,
+        y: 300,
+        image: countryUSAimage
+    }
+    let countryUK = {
+        x: 490,
+        y: 220,
+        image: countryUKimage
+    }
+    let countryIndia = {
+        x: 730,
+        y: 330,
+        image: countryIndiaimage
+    }
+    let countryAUS = {
+        x: 930,
+        y: 530,
+        image: countryAUSimage
+    }
+    
+    // CONTROL KEYS  
+    document.body.onkeydown = function(e){
+        if(e.keyCode == '38'){ //Move up
             avatar.y-=10
         }
-        if(e.key === 'd'){ //Move right
+        if(e.keyCode == '39'){ //Move right
             avatar.x+=10
         }
-        if(e.key === 'a'){ //Move left
+        if(e.keyCode == '37'){ //Move left
             avatar.x-=10
         }
-        if(e.key === 's'){ //Move down
+        if(e.keyCode == '40'){ //Move down
             avatar.y+=10; 
         }
-      }
+    }
       
-      
-      function animate(){       // Where the magic happens
+    function animate(){   
         frames++;  
-        ctx.clearRect(0, 0, canvas.width, canvas.height) //clears the canvas - flipping to a blank page
-          //drawSquare();
-          drawAvatar();
-          drawCountryUSA();
-          drawCountryUK();
-          drawCountryIn();
-          
-  
-          animateId = window.requestAnimationFrame(animate) //Game rendering -infinite loop that goes super fast
-      }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawAvatar();
+        recordPosition();
+        drawCovids();
+        drawCountryUSA();
+        drawCountryUK();
+        drawCountryIndia();
+        drawCountryAUS();
+        
+        animateId = window.requestAnimationFrame(animate);
+    }
       //animate()
-  
       //window.cancelAnimationFrame(animateId) to stop it
-  
       //requestAnimationFrame cancelAnimationFrame(id)
     
-  
     function startGame() {
         document.getElementById("start-button").remove();
     }
